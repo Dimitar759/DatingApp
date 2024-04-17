94ffc83d1283324372b277510f88f1b8a0ba8407
Dimitar759 <dimitarnikolov759@gmail.com>
2024-04-17 10:11:08 +0200

    Walking skeleton part 1

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options) // the base class is the db context
+    {
+    }
+
+
+    public DbSet <AppUser> Users { get; set; }
+}
