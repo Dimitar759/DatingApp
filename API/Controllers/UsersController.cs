34a340356fbbae168f84716641b9fbe459b8bff3
Dimitar759 <dimitarnikolov759@gmail.com>
2024-04-22 15:35:49 +0200

    Extending the api

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,8 @@
 ﻿﻿using API.Data;
+using API.DTOs;
 using API.Entities;
+using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -9,23 +12,28 @@ namespace API.Controllers;
 [Authorize]
 public class UsersController : BaseApiController
 {
-    private readonly DataContext _context;
+  private readonly IUserRepository _userRepository;
+  private readonly IMapper _mapper;
 
-    public UsersController(DataContext context)
-    {
-        _context = context;
-    }
+  public UsersController(IUserRepository userRepository, IMapper mapper)
+  {
+    _userRepository = userRepository;
+    _mapper = mapper;
+  }
 
-    [AllowAnonymous]
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
-    {
-        return await _context.Users.ToListAsync();
-    }
+  [HttpGet]
+  public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
+  {
+    var users = await _userRepository.GetMembersAsync();
 
-    [HttpGet("{id}")]
-    public async Task<ActionResult<AppUser>> GetUser(int id)
-    {
-        return await _context.Users.FindAsync(id);
-    }
+    return Ok(users);
+  }
+
+  [HttpGet("{username}")]
+  public async Task<ActionResult<MemberDto>> GetUser(string username)
+  {
+    return await _userRepository.GetMemberAsync(username);
+
+    
+  }
 }
--- API/Extensions/ApplicationServiceExtentions.cs
+++ API/Extensions/ApplicationServiceExtentions.cs
@@ -9,11 +9,15 @@ public static class ApplicationServiceExtensions
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
     {
-        services.AddScoped<ITokenService, TokenService>();
+        
         services.AddDbContext<DataContext>(opt =>
         {
             opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
         });
+        services.AddCors();
+        services.AddScoped<ITokenService, TokenService>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
         return services;
     }
