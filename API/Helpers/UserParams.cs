e7036374b33bfc70151b9032577a5e4cf50b6b9d
Dimitar759 <dimitarnikolov759@gmail.com>
2024-04-30 09:30:22 +0200

    sorting and filtering added

--- API/Helpers/UserParams.cs
+++ API/Helpers/UserParams.cs
@@ -20,6 +20,7 @@ namespace API.Helpers
        public string Gender { get; set; }
        public int MinAge { get; set; } = 18;
        public int MaxAge { get; set; } = 100;
+       public string OrderBy { get; set; } = "lastActive";
        
     }
 }
